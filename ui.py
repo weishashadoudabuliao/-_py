d7ad56f70d4264e398b7f312a2a88501ec382b44
weishashadoudabuliao <102843374+weishashadoudabuliao@users.noreply.github.com>
2022-05-09 11:49:20 +0800

    Add files via upload

---
+++ ui.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'untitledUUlVLS.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(688, 282)
+        self.label_2 = QLabel(Dialog)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(180, 170, 60, 16))
+        self.label = QLabel(Dialog)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(180, 100, 36, 16))
+        self.lineEdit = QLineEdit(Dialog)
+        self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setGeometry(QRect(250, 100, 133, 20))
+        self.pushButton = QPushButton(Dialog)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(310, 250, 75, 23))
+        self.plainTextEdit = QPlainTextEdit(Dialog)
+        self.plainTextEdit.setObjectName(u"plainTextEdit")
+        self.plainTextEdit.setGeometry(QRect(250, 160, 271, 31))
+
+        self.retranslateUi(Dialog)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"\u4e8c\u7ef4\u7801\u751f\u6210\u5668", None))
+        self.label_2.setText(QCoreApplication.translate("Dialog", u"\u56fe\u7247\u5730\u5740\uff1a", None))
+        self.label.setText(QCoreApplication.translate("Dialog", u"\u7f51\u5740\uff1a", None))
+        self.pushButton.setText(QCoreApplication.translate("Dialog", u"\u786e\u5b9a", None))
+    # retranslateUi
+
+
+
+
